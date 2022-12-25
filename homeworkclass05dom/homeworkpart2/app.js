d250eb45a1d6d09e5b184a44a83d715c3dc77b73
Ivan Ivanov <119753109+ivanovv7@users.noreply.github.com>
2022-12-25 11:35:15 +0100

    Update app.js

--- homeworkclass05dom/homeworkpart2/app.js
+++ homeworkclass05dom/homeworkpart2/app.js
@@ -7,20 +7,27 @@ let firstDiv = document.getElementById("firstDiv")
 
 let result = document.getElementById("result")
 
-//printing numbers from array
-firstDiv.innerHTML = `The numbers in the array are : ${arrayOfNumbers}`
+//printing numbers from array - POGRESEN KONCEPT
+//firstDiv.innerHTML = `The numbers in the array are : ${arrayOfNumbers}`
 
 //declare variale-helper for the sum operation (GLOBAL SCOPE)
 let sumOfArrayNum = 0
 
+firstDiv.innerHTML += "<ul>"
+
 for(let i = 0; i<arrayOfNumbers.length;i++){
+    //sum of all numbers in array using loop
     sumOfArrayNum+= arrayOfNumbers[i]
+    //printing numbers from array in OL - REFACTOR
+    firstDiv.innerHTML += `<li>${arrayOfNumbers[i]}</li>`
+
     
 }
+
+firstDiv.innerHTML += "</ul>"
 
 
 // printing result on html page
 result.innerHTML= `The sum of the numbers in the array is : ${sumOfArrayNum}`
 
 result.style.color="red"
-
